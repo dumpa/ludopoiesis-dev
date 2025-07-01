04691c4dfa756e1f711bf9107f45ef26c0f245ae
dumpa <juanpablocalderon@gmail.com>
2025-07-01 15:37:49 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 
+let idioma = localStorage.getItem("idioma") || "es";
+
+function setIdioma(id) {
+  idioma = id;
+  localStorage.setItem("idioma", idioma);
+  document.getElementById("idiomaToggle").checked = (idioma === "pt");
+  actualizarPantallaInicialConIdioma();
+}
+
+function toggleIdioma() {
+  const nuevo = (idioma === "es") ? "pt" : "es";
+  setIdioma(nuevo);
+}
+
+function actualizarPantallaInicialConIdioma() {
+  fetch("textos.json")
+    .then(res => res.json())
+    .then(data => {
+      const textos = data.inicio[idioma] || data.inicio["es"];
+      document.getElementById("inicio-subtitulo").innerText = textos.subtitulo;
+      document.getElementById("inicio-mensaje").innerText = textos.mensaje;
+      document.getElementById("inicio-boton-jugar").innerText = textos.jugar;
+    });
+}
+
 function mostrarPantalla(id) {
   ocultarTodasPantallas();
   document.getElementById(id).style.display = 'block';
